e0a34b86f91a4a9f8c8daa9c22acd6e09c52be1c
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2025-05-07 05:43:03 -0600

    few small fixes

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { motion, AnimatePresence } from 'framer-motion';
 import '../assets/header.sass';
 import logo from '../assets/logofd.png';
 
@@ -35,45 +36,57 @@ const Header = () => {
           aria-expanded="false"
           onClick={toggleMenu}
         >
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
-          <span aria-hidden="true"></span>
+          {isActive ? (
+            <i className="fas fa-times"></i>
+          ) : (
+            <i className="fas fa-bars"></i>
+          )}
         </button>
       </div>
 
-      <div className={`navbar-menu ${isActive ? 'is-active' : ''}`}>
-        <div className="navbar-start">
-          <Link className="navbar-item" to="/shop">
-            Shop
-          </Link>
-          <Link className="navbar-item" to="/sell">
-            Sell your device
-          </Link>
-          <Link className="navbar-item" to="/about">
-            About
-          </Link>
-          <Link className="navbar-item" to="/contact">
-            Contact
-          </Link>
-        </div>
-
-        <div className="navbar-end">
-          <div className="navbar-item">
-            <div className="buttons" style={{ gap: '0.5rem' }}>
-              <Link className="navbar-item" to="/cart">
-                <i className="fas fa-shopping-basket"></i>
-                {isMobile && <span className="cart-text"></span>}
+      <AnimatePresence>
+        {isActive && (
+          <motion.div 
+            className="navbar-menu"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={{ duration: 0.2 }}
+          >
+            <div className="navbar-start">
+              <Link className="navbar-item" to="/shop">
+                Shop
               </Link>
-              <Link className="button is-light is-outlined" to="/login">
-                Log in
+              <Link className="navbar-item" to="/sell">
+                Sell your device
               </Link>
-              <Link className="button is-light" to="/signup">
-                Sign up
+              <Link className="navbar-item" to="/about">
+                About
+              </Link>
+              <Link className="navbar-item" to="/contact">
+                Contact
               </Link>
             </div>
-          </div>
-        </div>
-      </div>
+
+            <div className="navbar-end">
+              <div className="navbar-item">
+                <div className="buttons" style={{ gap: '0.5rem' }}>
+                  <Link className="navbar-item" to="/cart">
+                    <i className="fas fa-shopping-basket"></i>
+                    {isMobile && <span className="cart-text"></span>}
+                  </Link>
+                  <Link className="button is-light is-outlined" to="/login">
+                    Log in
+                  </Link>
+                  <Link className="button is-light" to="/signup">
+                    Sign up
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </nav>
   );
 };
