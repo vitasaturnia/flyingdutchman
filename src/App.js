548d9a384e730667386deae39552ca327a5f2473
vitasaturnia <53528807+vitasaturnia@users.noreply.github.com>
2025-05-07 17:32:49 -0600

    css fix

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ const AnimatedPage = ({ children }) => {
       animate={{ opacity: 1 }}
       exit={{ opacity: 0 }}
       transition={{ duration: 0.5 }}
-      style={{ width: '100%', minHeight: '100vh' }}
+      style={{ width: '100%' }}
     >
       {children}
     </motion.div>
